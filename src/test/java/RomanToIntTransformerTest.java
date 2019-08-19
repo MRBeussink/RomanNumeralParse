d3a288b39fcd035486e4d761b62f4d4faf1efe04
Mark Beussink <c-Mark.Beussink@charter.com>
2019-08-19 14:39:56 -0500

    Cleaned up pom.xml, removed TestRunner.java

--- src/test/java/RomanToIntTransformerTest.java
+++ src/test/java/RomanToIntTransformerTest.java
@@ -10,10 +10,10 @@ public class RomanToIntTransformerTest {
 
         // assert statements
         assertEquals("III must be 3", 3, tester.parse("III"));
-        assertEquals("IV must be 3", 4, tester.parse("IV"));
-        assertEquals("IX must be 3", 9, tester.parse("IX"));
+        assertEquals("IV must be 4", 4, tester.parse("IV"));
+        assertEquals("IX must be 9", 9, tester.parse("IX"));
         assertEquals("LVIII must be 58", 58, tester.parse("LVIII"));
-        assertEquals("MCMXLIV", 1994, tester.parse("MCMXCIV"));
+        assertEquals("MCMXLIV must be 1994", 1994, tester.parse("MCMXCIV"));
     }
 
 
